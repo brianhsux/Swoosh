d0bb5a1d4e3c20ed09a6a9dbb093b4318bf941ae
Brian Hsu <brianhsux@gmail.com>
2018-03-13 22:27:59 +0800

    lesson-07-buttonUpTheActivities

---
+++ app/src/main/java/com/example/brian/swoosh/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.brian.swoosh
+
+/**
+ * Created by brian on 2018/3/13.
+ */
+
+const val EXTRA_LEAGUE = "league"
--- app/src/main/java/com/example/brian/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/brian/swoosh/LeagueActivity.kt
@@ -4,16 +4,44 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : AppCompatActivity() {
+
+    var leagueType = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
 
-    fun leagueNextClicked (view: View) {
-        val skillActivity = Intent(this, SkillActivity::class.java)
-        startActivity(skillActivity)
+    fun onMensClicked(view: View) {
+        leagueWomensBtn.isChecked = false
+        leagueCoedBtn.isChecked = false
+
+        leagueType = "Mens"
+    }
+
+    fun onWomensClicked(view: View) {
+        leagueMensBtn.isChecked = false
+        leagueCoedBtn.isChecked = false
+
+        leagueType = "Womens"
+    }
+
+    fun onCoedClicked(view: View) {
+        leagueMensBtn.isChecked = false
+        leagueWomensBtn.isChecked = false
+
+        leagueType = "Co-ed"
+    }
+
+    fun leagueNextClicked(view: View) {
+        if (leagueType != "") {
+            val skillActivity = Intent(this, SkillActivity::class.java)
+            skillActivity.putExtra(EXTRA_LEAGUE, leagueType)
+            startActivity(skillActivity)
+        }
+
     }
 }
